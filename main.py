5231c550212ea5be0c7e849d0fae274dfc3e443f
Bukola Testimony <102736653+Bukola-Testimony@users.noreply.github.com>
2022-07-11 20:44:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Write your code below this row 👇
+# write a program that calculates the sum of all the even numbers from 1 to 100. Thus, the first even number would be 2 and the last one is 100:
+
+total = 0
+for number in range(2,101,2):
+  total += number
+print(total)
+
+
+addition = 0
+for number in range(1,101):
+  if number % 2 == 0:
+   addition += number
+print(addition)
+
+
+add =2+4+6+8
+print(add)
+    
+
